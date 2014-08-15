c82c0649471add7c13330b2c4b2ba154127b9ec4
pandasa123 <pandasa123@gmail.com>
2014-08-15 13:56:06 -0400

    Update and rename cm.mk to slim.mk

--- slim.mk
+++ slim.mk
@@ -1,18 +1,18 @@
 $(call inherit-product, device/htc/m4/full_m4.mk)
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 PRODUCT_RELEASE_NAME := m4
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/m4/device.mk)
 
 # Device naming
 PRODUCT_DEVICE := m4
-PRODUCT_NAME := cm_m4
+PRODUCT_NAME := slim_m4
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One Mini
 PRODUCT_MANUFACTURER := HTC
